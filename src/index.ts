a4f656327088c48260940674a505840b8e2e3df8
Hrishikesh Addagatla <Hrishi0304>
2026-04-30 01:04:51 +0530

    Testing Global Error Handler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import pool from './config/db-config';
-import { tryCatch } from './utils/try-catch';
+import { errorHandler } from './middlewares/error-handlers';
+import { AppError } from './utils/app-error';
 
 dotenv.config();
 
@@ -11,8 +11,12 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({"message":"Server is ok"});
+  // res.json({"message":"Server is ok"});
+   // We are "throwing" a manual error to test our handler
+  throw new AppError("Testing our pro error handler!", 403);
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
