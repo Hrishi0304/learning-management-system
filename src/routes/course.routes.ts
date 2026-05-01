086c77dda3c02ea96773bb7e733ae3c3caa9ee80
Hrishikesh Addagatla <Hrishi0304>
2026-05-01 11:59:28 +0530

    Login/Signup complete

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -6,14 +6,15 @@ import {
     updateCourse,
     deleteCourse
 } from '../controllers/course.controllers';
+import { authenticate } from '../middlewares/authenticate';
 
 const router = Router();
 
 // Map each HTTP method + path → controller function
-router.get('/', getAllCourses);
-router.get('/:id', getCourseById);
-router.post('/', createCourse);
-router.put('/:id', updateCourse);
-router.delete('/:id', deleteCourse);
+router.get('/',authenticate,getAllCourses);
+router.get('/:id',authenticate, getCourseById);
+router.post('/',authenticate, createCourse);
+router.put('/:id',authenticate, updateCourse);
+router.delete('/:id',authenticate, deleteCourse);
 
 export default router;
